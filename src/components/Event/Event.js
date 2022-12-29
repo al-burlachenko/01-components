49ba420b0cfb8fbd2fd64dd32339e6beecfe0e1f
Alexandr <alexander@burlachenko>
2022-12-29 16:04:16 +0200

    adds css-variables, utils and nearly finishes events component

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,30 +1,30 @@
 import PropTypes from 'prop-types';
+import { FaCalendarAlt, FaClock, FaMapMarkerAlt, FaUserAlt } from 'react-icons/fa';
+import { formatDate, formatEventDistance } from 'utils';
 import css from './Event.module.css';
 
-import { FaCalendarAlt, FaClock, FaMapMarkerAlt, FaUserAlt } from 'react-icons/fa';
-
-// console.log(s);
+console.log(css);
 function Event({ name, location, speaker, type, timeStart, timeEnd }) {
   return (
     <div className={css.event}>
       <h2 className={css.title}>Name</h2>
       <p className={css.info}>
-        <FaMapMarkerAlt className={css.icon} size={16}></FaMapMarkerAlt>
+        <FaMapMarkerAlt className={css.icon} size={16} />
         {location}
       </p>
       <p className={css.info}>
-        <FaUserAlt className={css.icon} size={16}></FaUserAlt>
+        <FaUserAlt className={css.icon} size={16} />
         {speaker}
       </p>
       <p className={css.info}>
-        <FaCalendarAlt className={css.icon} size={16}></FaCalendarAlt>
-        {timeStart}
+        <FaCalendarAlt className={css.icon} size={16} />
+        {formatDate(timeStart)}
       </p>
       <p className={css.info}>
-        <FaClock className={css.icon} size={16}></FaClock>
-        Duration
+        <FaClock className={css.icon} size={16} />
+        {formatEventDistance(timeStart, timeEnd)}
       </p>
-      <span className={css.chip}>Event type</span>
+      <span className={`${css.chip} ${css[type]}`}>{type}</span>
     </div>
   );
 }
