2422fab8389a5e8a19921f6c865dd97da0b0a855
Alexandr <alexander@burlachenko>
2022-12-29 17:00:28 +0200

    fixes bugs and adds constants

--- src/components/EventBoard/EventBoard.js
+++ src/components/EventBoard/EventBoard.js
@@ -29,10 +29,10 @@ EventBoard.propTypes = {
       location: PropTypes.string.isRequired,
       speaker: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
-      time: {
+      time: PropTypes.exact({
         start: PropTypes.string.isRequired,
         end: PropTypes.string.isRequired,
-      },
+      }),
     }),
   ),
 };
